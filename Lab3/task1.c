5909cf2f90110915c7812521289072807beacfb3
Alexander Nachtigal <a.nachtigal98@gmail.com>
2022-04-26 14:59:29 +0200

    initial commit

---
+++ Lab3/task1.c
@@ -0,0 +1,15 @@
+//
+// Created by Alex on 26.04.2022.
+//
+
+struct beispiel {
+    int daten;
+    struct beispiel *pNext;
+};
+
+int main(void) {
+    struct beispiel element1;
+    struct beispiel element2;
+    element1.pNext =
+    return 0;
+}
