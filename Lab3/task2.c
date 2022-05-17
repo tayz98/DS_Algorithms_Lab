7ccd52ddad74f017e69c9051548159e5514b42f6
Alexander Nachtigal <a.nachtigal98@gmail.com>
2022-05-17 15:21:42 +0200

    added insert function, it works :)

--- Lab3/task2.c
+++ Lab3/task2.c
@@ -15,7 +15,7 @@ typedef struct listnode {
 } Listnode;
 
 void print_list(Listnode*, int);
-void insert_element(Listnode*, int);
+void insert_element(Listnode*, int, int);
 int main(void) {
 
     // create elements and fill them with data
@@ -36,7 +36,8 @@ int main(void) {
     pStart->next->next->next->next->next->next->value = 70;
     pStart->next->next->next->next->next->next->next = NULL;
     print_list(pStart, 3);
-    insert_element(pStart, 2);
+    insert_element(pStart, 2, 100);
+    print_list(pStart,7);
     return 0;
 }
 
@@ -49,12 +50,13 @@ void print_list(Listnode *pStart, int count) {
     }
 }
 
-void insert_element(Listnode *pStart, int position) {
-    Listnode  *pTemp;
-    for (int i = 1; i<=position; i++) {
-        pTemp = pTemp->next;
+void insert_element(Listnode *pStart, int position, int val) {
+    Listnode  *pTemp, *p; // pTemp is the run variable, p is the new element.
+    p->value = val;
+    pTemp = pStart; // set the run variable to the address of pStart.
+    for (int i = 1; i<position-1; i++) {
+        pTemp = pTemp->next; // it loops until we are one element away of the given position variable.
     }
-
-    pStart -> next = pTemp -> next;
-    pTemp -> next = pStart;
+    p->next = pTemp->next; // p->next shows to the same next element as pTemp->next.
+    pTemp->next = p; // let pTemp->next be the address of the p with new value.
 }
